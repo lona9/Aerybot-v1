86ec169a5133173c3223392753f62b6d2fccae70
Pilar Vásquez <pilar.vasquez.h@gmail.com>
2021-01-29 23:23:14 +0000

    update champ files from D to I

--- main.py
+++ main.py
@@ -3,46 +3,69 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 import asyncio
-from keep_alive import keep_alive 
+from keep_alive import keep_alive
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="&")
+bot = commands.Bot(command_prefix="aery ")
 
 #FOR ARAM STATS
 @bot.command()
 async def aram(ctx, *args):
-  charname = "".join(args).lower()
-
+  charname = "".join(args).lower().replace("'", "").replace(".", "")
+  
   def filename(x):
-    arampath = '/home/runner/lolbot/aram/'
+    arampath = '/home/runner/Aerybot/aram/'
     extension = '.txt'
     file = arampath + x + extension
     return file
-
+    
   path = filename(charname)
   with open(path) as f:
     text = f.read()
     await ctx.channel.send(text)
+
 
 #FOR NORMAL/RANKED STATS
 @bot.command()
 async def normal(ctx, *args):
-  charname = "".join(args).lower()
-
+  charname = "".join(args).lower().replace("'", "").replace(".", "")
+  
   def filename(x):
-    normalpath = '/home/runner/lolbot/normal/'
+    normalpath = '/home/runner/Aerybot/normal/'
     extension = '.txt'
     file = normalpath + x + extension
     return file
-
+    
   path = filename(charname)
-
+  
   with open(path) as f:
     text = f.read()
     await ctx.channel.send(text)
+
+@bot.command()
+async def info(ctx):
+	with open("info.txt") as f:
+		text = f.read()
+		await ctx.channel.send(text)
+
+
+@bot.command()
+async def comandos(ctx):
+	with open("comandos.txt") as f:
+		text = f.read()
+		await ctx.channel.send(text)
+
+
+@bot.command()
+async def rotacion(ctx):
+	with open("rotacion.txt") as f:
+		text = f.read()
+		await ctx.channel.send(
+		    '**Los champs en rotación de esta semana son:** \n{}'.format(text))
+
 
 keep_alive()
 
