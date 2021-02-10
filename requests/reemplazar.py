2a362941456688b636a0954c082f0f59f5a027b2
Pilar <pilar.vasquez.h@gmail.com>
2021-02-10 22:30:56 +0000

    replace different build names due to translation

--- requests/reemplazar.py
+++ requests/reemplazar.py
@@ -1,14 +1,17 @@
+import os
+
 ParaReemplazar = ['Carcaj del mediodía', 'Guardián invisible', 'Daga de hechicero', 'Espada de granizo', 'Cuchillo de ascuas', 'Recaudadora', 'Sombrero mortal de Rabadon', 'Viento huracanado', 'Ángel de la guarda', 'Filo infinito', 'Recordatorio letal', 'Grebas del berseker', 'Botas blindadas', 'Botas de hechicero', 'Látigo puntahierro', 'Chupasangre', 'Calibrador de Sterak', 'Rostro espiritual', 'Mecanoespada punki', 'Fuerza de trinidad', 'Perdición del liche', 'Bastón del vacío', 'Medallón de los Solari de Hierro', 'Promesa de caballero', 'Capa solar', 'Malla de espinas', 'Protector pétreo de gárgola', 'Desconsuelo de Liandry', 'Bastón del arcángel', 'Incensario ardiente', 'Bastón de aguas fluidas', 'Gema avivadora', 'Desgarrador divino', 'Baile de la muerte']
 
 ReemplazarPor = ['Carcaj de mediodía', 'Centinela invisible', 'Filo del Robahechizos', 'Hoja granizo', 'Cuchillo Ámbar', 'El Coleccionista', 'Sombrero mortífero de Rabadon', 'Fuerza del Viento', 'Ángel Guardián', 'Filo del infinito', 'Recordatorio mortal', 'Grebas de berserker', 'Punteras de acero revestidas', 'Botas del hechicero', 'Látigo férreo', 'Bebedor de sangre', 'Guantelete de Sterak', 'Apariencia espiritual', 'Sierraespada Quimopunk', 'Fuerza de la trinidad', 'Maldición del liche', 'Báculo del vacío', 'Relicario de los Solari de Hierro', 'Promesa del caballero', 'Égida de fuego solar', 'Cota de espinas', 'Armadura pétrea', 'Angustia de Liandry', 'Báculo del arcángel', 'Pebetero ardiente', 'Báculo de agua fluyente', 'Gemaluz', 'Cercenador divino', 'Danza de la muerte']
 
-for champ in os.listdir('requests'):
+for champ in os.listdir('.'):
   print(champ)
   original = open(champ, 'r')
   reemplazo = original.read()
-  for i in len(ParaRemplazar):
-    reemplazo = reemplazo.replace(ParaRemplazar[i], ReemplazarPor[i])
+  for i in range(len(ParaReemplazar)):
+    reemplazo = reemplazo.replace(ParaReemplazar[i], ReemplazarPor[i])
   original.close()
   original = open(champ, 'w')
   original.write(reemplazo)
+  original.truncate()
   original.close()
