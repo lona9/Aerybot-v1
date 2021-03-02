b9f8fc4f163b3c0174500be5550f0b0a449d8bf3
Pilar <pilar.vasquez.h@gmail.com>
2021-03-02 19:16:18 +0000

    create files for normal mode champs, first bulk

--- normals/normal.py
+++ normals/normal.py
@@ -8,8 +8,8 @@ for champ in fix:
     filename = champ + '.txt'
     filestofix.append(filename)
 
-for champ in filestofix:
-# for champ in os.listdir('files'):
+# for champ in filestofix:
+for champ in os.listdir('files'):
   with open(os.path.join('files', champ)) as file:
     print(champ, 'iniciando')
     soup = BeautifulSoup(file, features='html5lib')
@@ -490,7 +490,7 @@ for champ in filestofix:
 
     ## BOTAS
     botas = soup.find_all('div', "championSpell")
-    imgbotas = botas[13].find('img', alt=True)
+    imgbotas = botas[10].find('img', alt=True)
     botas = imgbotas.get('alt')
 
     ### OBJETOS PRINCIPALES
@@ -500,7 +500,7 @@ for champ in filestofix:
       imgprincipales = principales[obj].find('img', alt=True)
       principalts = imgprincipales.get('alt')
       objs.append(principalts)
-    objpr = objs[9:13]
+    objpr = objs[6:10]
     objpr = " > ".join(objpr)
 
     texto = ("**{}** *(Normal)*\n**Posición:** *{}*\n**Porcentaje de victorias:** {}\n**Hechizos del Invocador:** {} {}\n**Runas:** {} {}\n\n>{}\n>{}\n>{}\n>{}\n\n>{}\n>{}\n>{}\n>   {} {} {}\n\n**Build**\n**Botas:** {}\n**Objetos iniciales:** {}\n**Objetos principales:** {}\n**Objetos finales:** {}".format(nombre, posicion, victorias, hechizo1, hechizo2, runa1, runa2, linea1pr, linea2pr, linea3pr, linea4pr, linea2sec, linea3sec, linea4sec, fragmento1, fragmento2, fragmento3, botas, objin, objpr, objfin))
